c5d1b2aa55ec9019aa9d3d6a02468a385abd20a2
Nastia Kuzina <miowch@gmail.com>
2022-02-02 17:55:19 +0100

    Ex5 JSON Parsing

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import requests
+import json
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+
+obj = json.loads(json_text)
+
+print(obj["messages"][1]["message"])
+
