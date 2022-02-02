70c9078de94492e6f3ca9201035065ba03c5a252
Nastia Kuzina <miowch@gmail.com>
2022-02-02 18:15:53 +0100

    Ex6 Several redirections

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
+import requests
 
-json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 
-obj = json.loads(json_text)
-
-print(obj["messages"][1]["message"])
-
+print(str(response.history).count("301"))
+print(response.url)
