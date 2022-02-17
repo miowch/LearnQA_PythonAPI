ebf4ce09ee68ab5233525c017a6195ad32d8a192
Nastia Kuzina <miowch@gmail.com>
2022-02-17 18:19:41 +0100

    l4m4 Edit user

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,24 +1,11 @@
-from datetime import datetime
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 
 class TestUserRegister(BaseCase):
-    def setup(self):
-        base_part = "learnqa"
-        domain = "example.com"
-        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
-
     def test_create_user_successfully(self):
-        data = {
-            'password': '123',
-            'username': 'leatrnqa',
-            'firstName': 'leatrnqa',
-            'lastName': 'leatrnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
 
         response = requests.post(
             "https://playground.learnqa.ru/api/user/",
@@ -30,13 +17,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'password': '123',
-            'username': 'leatrnqa',
-            'firstName': 'leatrnqa',
-            'lastName': 'leatrnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email)
 
         response = requests.post(
             "https://playground.learnqa.ru/api/user/",
